ac5152ef49c4fe2ea91e6f9bebfd81d5acc7fbdb
bagwanbulbul <bulbulbagwan918@gmail.com>
2019-12-27 10:41:51 +0530

    some changes in resolve function

--- promise.js
+++ promise.js
@@ -10,7 +10,7 @@ var promise = new Promise(function(resolve, reject) {
   
 promise. 
     then(function () { 
-        console.log('Success, You are a GEEK'); 
+        console.log('Success.....'); 
     }). 
     catch(function () { 
         console.log('Some error has occured'); 
